7b39d2952e31e36d4f71db3a89c7facef68f69d3
edgarsanchez01 <a3521110095@alumno.uttehuacan.edu.mx>
2022-10-07 22:01:48 -0500

    tercera modificacion 7 octubre

--- src/assets/components/Formulario.vue
+++ src/assets/components/Formulario.vue
@@ -1,28 +1,67 @@
+<script>
+import ProgressBar from './ProgressBar.vue';
+import TotalProyectos from './TotalProyectos.vue';
+import TotalProyectos1 from './TotalProyectos.vue';
+export default {
+    data: () => ({
+        proyecto: "",
+        tipo: "",
+        urgente: false,
+        proyectos: [],
+    }),
+    methods: {
+        registrarProyecto() {
+            const proyecto = {
+                proyecto: this.proyecto,
+                tipo: this.tipo,
+                urgente: this.urgente,
+                completado: false,
+            };
+            this.proyectos.push(proyecto);
+            this.proyecto = "";
+            this.tipo = "";
+            this.urgente = false;
+        },
+        cambiarEstado(proyecto, campo) {
+            // el signo de admiracion lo cambia
+            // this.proyectos[id].urgente = !this.proyectos[id].urgente;
+            // console.log(proyecto, campo);
+            proyecto[campo] = !proyecto[campo];
+        }
+    },
+    // propiedades computadas
+    computed: {
+        numeroProyectos() {
+            return this.proyectos.length;
+        },
+        porcentaje() {
+            let completados = 0;
+            this.proyectos.map(proyecto => {
+                if (proyecto.completado)
+                    completados++;
+            });
+            return (completados * 100) / this.numeroProyectos || 0;
+        }
+    },
+    components: { ProgressBar, TotalProyectos, TotalProyectos1 }
+};
+</script>
 <template>
 
     <div class="row">
         <div class="col-12 mb-4">
-            <h3 class="text-center" > Proceso: 0%  </h3>
-            <div class="progress">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" 
-                role="progressbar" 
-                aria-valuenow="25" 
-                aria-valuemin="0" 
-                aria-valuemax="100"
-                style="width: 25%"
-                > </div>
-            </div>
+            <progress-bar :porcentaje="porcentaje"/>
         </div>
 
         <div class="col-12 col-md-4">
             <form @submit.prevent="registrarProyecto">
                 <div class="mb-3">
-                    <label  class="form-label">Proyecto</label>
+                    <label class="form-label">Proyecto</label>
                     <input v-model.trim="proyecto" type="text" class="form-control" required />
                 </div>
 
                 <div class="mb-3">
-                    <label  class="form-label">Actividad</label>
+                    <label class="form-label">Actividad</label>
                     <select v-model="tipo" class="form-select" required>
                         <option disabled selected value="">Selecciona un tipo de actividad</option>
                         <option>Aplicaciones Wen con Vue.js</option>
@@ -33,99 +72,19 @@
 
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-check-label">Urgente</label>
-                    <input v-model="urgente" type="checkbox" class="form-check-input"/>
+                    <input v-model="urgente" type="checkbox" class="form-check-input" />
                 </div>
 
 
-            <button type="submit" class="btn btn-primary">Guardar</button>
-            </form>  
+                <button type="submit" class="btn btn-primary">Guardar</button>
+            </form>
         </div>
         <div class="col-12 col-md-8">
             <hr>
-            <h3>
-                <!-- propiedades calculadas| computadas -->
-                Total Proyectos: {{ numeroProyectos }}
-            </h3>
-            <!-- <pre>
-                {{ proyecto }}
-                {{ tipo }}
-                {{ urgente }}
-            </pre> -->
-
-            <div class="table-responsive">
-                <!-- el over nos sirve para pintar -->
-                <table class="table table-dark table-hover">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Proyecto</th>
-                            <th>Tipo</th>
-                            <th>Urgente</th>
-                            <th>Completado</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr v-for="(proyecto, index) in proyectos" :key="index">
-                            <td>{{ index+1 }}</td>
-                            <td> {{ proyecto.proyecto }} </td>
-                            <td> {{ proyecto.tipo }} </td>
-                            <!-- operador ternario -->
-                            <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'"> {{ proyecto.urgente ? "Si" : "No" }} </td>
-                            <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'"> {{ proyecto.completado ? "Completo" : "Incompleto" }} </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-
+            <total-proyectos :numeroProyectos="numeroProyectos" :proyectos="proyectos" :cambiarEstado="cambiarEstado"/>
         </div>
     </div>
 
-    
-    
 </template>
 
-<script>
-    export default{
-        data: () => ({
-            proyecto: "",
-            tipo: "",
-            urgente: false,
-            proyectos: [],
-            
-        }),
-        methods:{
-            registrarProyecto(){
-                const proyecto = {
-                    proyecto: this.proyecto,
-                    tipo: this.tipo,
-                    urgente: this.urgente,
-                    completado: false,
-                };
-                this.proyectos.push(proyecto);
-
-                
-
-                this.proyecto = "";
-                this.tipo = "";
-                this.urgente = false;
-            },
-
-            cambiarEstado(proyecto, campo){
-                // el signo de admiracion lo cambia
-                // this.proyectos[id].urgente = !this.proyectos[id].urgente;
-                // console.log(proyecto, campo);
-                proyecto[campo] = !proyecto[campo];
-            }
-
-        },
-        // propiedades computadas
-        computed:{
-            numeroProyectos(){
-                return this.proyectos.length;
-            },
-        },
-    };
-</script>
 
